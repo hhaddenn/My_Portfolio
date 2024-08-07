e8a5dc98d78bd7393a752c3cb96b35c528b692c6
hhaddenn <hugohaddenn@gmail.com>
2024-08-07 15:33:28 +0100

    Created MyPortfolio

--- main.py
+++ main.py
@@ -2,15 +2,37 @@ import flask
 from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 import os
+from flask_mail import Mail, Message
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/email', methods=['POST'])
+def email():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form['subject']
+    message = request.form['message']
+
+    msg = Message(subject, sender=email, recipients=[os.environ.get('MAIL_USERNAME')])
+    msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    mail.send(msg)
+
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
