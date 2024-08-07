ba69ea7a592d635b3bd22cbb53418b9dfbd5bf3b
hhaddenn <hugohaddenn@gmail.com>
2024-08-07 11:13:22 +0100

    Main changed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def home():
